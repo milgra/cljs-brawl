e0d8075b763ce944041ad3bf0b1a75498ac62c9f
Milan Toth <milgra@Milans-MacBook.local>
2019-10-15 22:35:27 +0200

    fixes

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [org.clojure/core.async  "0.4.500"]]
+                 [org.clojure/core.async  "0.4.500"]
+                  [cljs-http "0.1.46"]]
 
   :plugins [[lein-figwheel "0.5.19"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
