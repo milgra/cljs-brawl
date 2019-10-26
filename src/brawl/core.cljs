b48902ab15601b05aad84981dc2692476c31e2bf
Milan Toth <milgra@milgra.com>
2019-10-26 16:57:36 +0200

    webgl namespace added

--- src/brawl/core.cljs
+++ src/brawl/core.cljs
@@ -5,67 +5,15 @@
             [cljs-http.client :as http]
             [cljs.core.async :refer [<! chan put! take! poll!]]
             [cljs.core.async :refer-macros [go]]
-            [cljs-webgl.context :as context]
-            [cljs-webgl.shaders :as shaders]
-            [cljs-webgl.constants.draw-mode :as draw-mode]
-            [cljs-webgl.constants.data-type :as data-type]
-            [cljs-webgl.constants.buffer-object :as buffer-object]
-            [cljs-webgl.constants.shader :as shader]
-            [cljs-webgl.buffers :as buffers]
-            [cljs-webgl.typed-arrays :as ta]
             [brawl.surface :as surface]
             [brawl.svg :as svg]
             [brawl.physics :as physics]
             [brawl.mass :as mass]
             [brawl.math4 :as math4]
-            [brawl.shape :as shape])
+            [brawl.shape :as shape]
+            [brawl.webgl :as webgl])
   (:import [goog.events EventType]))
   
-;;(println "AA This text is printed from src/brawl/core.cljs. Go ahead and edit it and see reloading in action.")
-
-
-(def vertex-source
-  "attribute highp vec4 position;
-   attribute highp vec4 color;
-   varying highp vec4 colorv;
-   varying highp vec4 positionv;
-   uniform mat4 projection;
-   void main ( )
-   {
-	gl_Position = projection * position;
-	gl_PointSize = 8.0;
-	colorv = color;
-	positionv = position;
-   }")
-
-
-(def fragment-source
-  "varying highp vec4 colorv;
-   varying highp vec4 positionv;
-   void main( )
-   {
-	gl_FragColor = colorv;
-   	if ( colorv.w == 1.0 && colorv.x < 0.8 )
-   	{
-       	 highp float ver = sin(positionv.y)+1.0;
-       	 highp float hor = sin(positionv.x)+1.0;
-       	 highp float dia = sin((positionv.x+positionv.y)/1.5)+1.0;
-       	 ver = floor(ver * 2.0)/4.0;
-       	 hor = floor(hor * 2.0)/4.0;
-       	 dia = floor(dia * 2.0)/4.0;
-       	 if ( colorv.x >= colorv.y && colorv.x >= colorv.z ) gl_FragColor.x -= ver*0.05;
-       	 if ( colorv.y >= colorv.x && colorv.y >= colorv.z ) gl_FragColor.y -= hor*0.05;
-	 if ( colorv.z >= colorv.x && colorv.z >= colorv.y ) gl_FragColor.z -= dia*0.2;
-   	}
-   }")
-
-
-(defn gen-vertex-triangle [ vertexes color ]
-  (let [r ( / (bit-shift-right (bit-and color 0xFF0000) 16) 255.0 )
-        g ( / (bit-shift-right (bit-and color 0x00FF00) 8) 255.0 )
-        b ( / (bit-and color 0x0000FF) 255.0 ) ]
-    (map #( concat % [0.0 1.0 r g b 1.0] ) vertexes )))
-
 
 (defn load-level! [channel name]
   (go
@@ -73,19 +21,12 @@
                                  ;; parameters
                                  {:with-credentials? false}))
           xmlstr (xml->clj (:body response) {:strict false})
-          level (svg/psvg xmlstr "")
-          shapes level ;; (filter #(not= (% :id) "Surfaces") level)
-          surfacepoints (filter #(= (% :id) "Surfaces") level)
-          surfaces (surface/generate-from-pointlist surfacepoints)
-          vertexes (flatten
-                     ( map
-                      (fn [shape]
-                        (if (contains? shape :color)
-                          ( gen-vertex-triangle (shape/triangulate_c (:path shape) ) (:color shape ))))
-                      shapes))]
+          shapes (svg/psvg xmlstr "")]
+      ;;shapes level ;; (filter #(not= (% :id) "Surfaces") level)
+      ;;surfacepoints (filter #(= (% :id) "Surfaces") level)
+      ;;surfaces (surface/generate-from-pointlist surfacepoints)
       ;;(for [x (range 0 10)] (p/mass2 (rand 1000) (rand 1000)))]
-      (println "map loaded")
-      (put! channel vertexes))))
+      (put! channel shapes))))
 
 
 (defn animate [state draw-fn]
@@ -98,31 +39,10 @@
 
 
 (defn main []
-  (println "main") 
+
   (let
-      [context (context/get-context (.getElementById js/document "main"))
-
-       shader (shaders/create-program
-               context
-               (shaders/create-shader context shader/vertex-shader vertex-source)
-               (shaders/create-shader context shader/fragment-shader fragment-source))
-
-       scene_buffer (buffers/create-buffer
-                     context
-                     (ta/float32 [ 500.0 500.0 0.0 1.0 1.0 1.0 1.0 1.0 ])
-                     buffer-object/array-buffer
-                     buffer-object/static-draw)
-
-       actor_buffer (buffers/create-buffer
-                     context
-                     (ta/float32 [ 500.0 500.0 0.0 1.0 1.0 1.0 1.0 1.0 ])
-                     buffer-object/array-buffer
-                     buffer-object/dynamic-draw)
-       
-       location_pos (shaders/get-attrib-location context shader "position")
-       location_col (shaders/get-attrib-location context shader "color")
-
-       initstate {:level_file "level0.svg"
+      [initstate {:glstate (webgl/init)
+                  :level_file "level0.svg"
                   :level_state "none"
                   :keypresses {}
                   :trans [500.0 500.0]
@@ -157,20 +77,13 @@
            )
          
          (= (:level_state state) "loading")
-         (let [vertexes (poll! filechannel)]
-           (if vertexes
-             (do
-               (println "vertexes arrived")
-               (.bindBuffer context buffer-object/array-buffer scene_buffer)
-               (.bufferData context
-                            buffer-object/array-buffer
-                            (ta/float32 vertexes)
-                            buffer-object/static-draw)
-               (-> state
-                   (assoc :level_state "loaded")
-                   (assoc :vertexes vertexes)))
+         (let [shapes (poll! filechannel)]
+           (if shapes
+             (-> state
+                 (assoc :glstate (webgl/loadshapes shapes))
+                 (assoc :level_state "loaded")
              state))
-
+  
          (= (:level_state state) "loaded")
          (let [[tx ty] (:trans state)
                [sx sy] (:speed state)
@@ -183,70 +96,11 @@
                            -1.0 1.0)
 
                keyevent (poll! keychannel)]
-           
-           (buffers/clear-color-buffer context 0.1 0.0 0 1)
-           
-           ;; (actors/update actor controlstate)
-           
-           ;; draw scene buffer
-           
-           (.bindBuffer context buffer-object/array-buffer scene_buffer)
-           
-           (buffers/draw!
-            context
-            :count (/ (count (:vertexes state)) 8)
-            :shader shader
-            :draw-mode draw-mode/triangles               
-            :attributes [{:buffer scene_buffer
-                          :location location_pos
-                          :components-per-vertex 4
-                          :type data-type/float
-                          :offset 0
-                          :stride 32}
-                         {:buffer scene_buffer
-                          :location location_col
-                          :components-per-vertex 4
-                          :type data-type/float
-                          :offset 16
-                          :stride 32}]
-            :uniforms [{:name "projection"
-                        :type :mat4
-                        :values projection}])
-           
-           ;; draw actor buffer
-           
-           (.bindBuffer context buffer-object/array-buffer actor_buffer)
-           
-           ;; load in new vertexdata
-           
-           (.bufferData context
-                        buffer-object/array-buffer
-                        (ta/float32 [tx ty 0.0 1.0 1.0 1.0 1.0 1.0])
-                        buffer-object/dynamic-draw)
-           
-           (buffers/draw!
-            context
-            :count 1
-            :shader shader
-            :draw-mode draw-mode/points               
-            :attributes [{:buffer actor_buffer
-                          :location location_pos
-                          :components-per-vertex 4
-                          :type data-type/float
-                          :offset 0
-                          :stride 32}
-                         {:buffer actor_buffer
-                          :location location_col
-                          :components-per-vertex 4
-                          :type data-type/float
-                          :offset 16
-                          :stride 32}]
-            :uniforms [{:name "projection"
-                        :type :mat4
-                        :values projection}]
-            )
 
-         
+           (webgl/draw (:glstate state) projection (:trans state))
+
+           ;; (actors/update actor controlstate)
+
            ;; handle keypresses, modify main point trans
 
            (let [keycodes (if keyevent
@@ -284,6 +138,8 @@
 (main)
 
 ;; template functions
+
+;;(println "AA This text is printed from src/brawl/core.cljs. Go ahead and edit it and see reloading in action.")
 
 (def app-state (atom {}))
 
---
+++ src/brawl/webgl.cljs
@@ -0,0 +1,157 @@
+(ns brawl.webgl
+  (:require [cljs-webgl.context :as context]
+            [cljs-webgl.shaders :as shaders]
+            [cljs-webgl.constants.draw-mode :as draw-mode]
+            [cljs-webgl.constants.data-type :as data-type]
+            [cljs-webgl.constants.buffer-object :as buffer-object]
+            [cljs-webgl.constants.shader :as shader]
+            [cljs-webgl.buffers :as buffers]
+            [cljs-webgl.typed-arrays :as ta]))
+  
+(def vertex-source
+  "attribute highp vec4 position;
+   attribute highp vec4 color;
+   varying highp vec4 colorv;
+   varying highp vec4 positionv;
+   uniform mat4 projection;
+   void main ( )
+   {
+	gl_Position = projection * position;
+	gl_PointSize = 8.0;
+	colorv = color;
+	positionv = position;
+   }")
+
+(def fragment-source
+  "varying highp vec4 colorv;
+   varying highp vec4 positionv;
+   void main( )
+   {
+	gl_FragColor = colorv;
+   	if ( colorv.w == 1.0 && colorv.x < 0.8 )
+   	{
+       	 highp float ver = sin(positionv.y)+1.0;
+       	 highp float hor = sin(positionv.x)+1.0;
+       	 highp float dia = sin((positionv.x+positionv.y)/1.5)+1.0;
+       	 ver = floor(ver * 2.0)/4.0;
+       	 hor = floor(hor * 2.0)/4.0;
+       	 dia = floor(dia * 2.0)/4.0;
+       	 if ( colorv.x >= colorv.y && colorv.x >= colorv.z ) gl_FragColor.x -= ver*0.05;
+       	 if ( colorv.y >= colorv.x && colorv.y >= colorv.z ) gl_FragColor.y -= hor*0.05;
+	 if ( colorv.z >= colorv.x && colorv.z >= colorv.y ) gl_FragColor.z -= dia*0.2;
+   	}
+   }")
+
+
+(defn gen-vertex-triangle [ vertexes color ]
+  (let [r ( / (bit-shift-right (bit-and color 0xFF0000) 16) 255.0 )
+        g ( / (bit-shift-right (bit-and color 0x00FF00) 8) 255.0 )
+        b ( / (bit-and color 0x0000FF) 255.0 ) ]
+    (map #( concat % [0.0 1.0 r g b 1.0] ) vertexes )))
+
+
+(defn init []
+  {:mycontext (context/get-context (.getElementById js/document "main"))
+
+   :shader (shaders/create-program
+            mycontext
+            (shaders/create-shader context shader/vertex-shader vertex-source)
+            (shaders/create-shader context shader/fragment-shader fragment-source))
+   
+   :scene_buffer (buffers/create-buffer
+                  mycontext
+                  (ta/float32 [ 500.0 500.0 0.0 1.0 1.0 1.0 1.0 1.0 ])
+                  buffer-object/array-buffer
+                  buffer-object/static-draw)
+   
+   :actor_buffer (buffers/create-buffer
+                  mycontext
+                  (ta/float32 [ 500.0 500.0 0.0 1.0 1.0 1.0 1.0 1.0 ])
+                  buffer-object/array-buffer
+                  buffer-object/dynamic-draw)
+   
+   :location_pos (shaders/get-attrib-location context shader "position")
+   :location_col (shaders/get-attrib-location context shader "color")})
+
+
+(defn loadshapes [:keys [context] state shapes]
+  (let [vertexes (flatten
+                  (map
+                   (fn [shape]
+                     (if (contains? shape :color)
+                       ( gen-vertex-triangle (shape/triangulate_c (:path shape) ) (:color shape ))))
+                   shapes))]
+    
+    (.bindBuffer context buffer-object/array-buffer (:scene_buffer state))
+    (.bufferData context
+                 buffer-object/array-buffer
+                 (ta/float32 vertexes)
+                 buffer-object/static-draw)
+                 
+    (assoc state :vertexes vertexes)))
+
+
+(defn draw [{:keys [context shader scene_buffer actor_buffer location_pos location_col] :as state} projection [tx ty]]
+             
+  (buffers/clear-color-buffer context 0.1 0.0 0 1)
+    
+  ;; draw scene buffer
+  
+  (.bindBuffer context buffer-object/array-buffer scene_buffer)
+  
+  (buffers/draw!
+   context
+   :count (/ (count (:vertexes state)) 8)
+   :shader shader
+   :draw-mode draw-mode/triangles               
+   :attributes [{:buffer scene_buffer
+                 :location location_pos
+                 :components-per-vertex 4
+                 :type data-type/float
+                 :offset 0
+                 :stride 32}
+                {:buffer scene_buffer
+                 :location location_col
+                 :components-per-vertex 4
+                 :type data-type/float
+                 :offset 16
+                 :stride 32}]
+   :uniforms [{:name "projection"
+               :type :mat4
+               :values projection}])
+  
+  ;; draw actor buffer
+  
+  (.bindBuffer context buffer-object/array-buffer actor_buffer)
+  
+  ;; load in new vertexdata
+  
+  (.bufferData context
+               buffer-object/array-buffer
+               (ta/float32 [tx ty 0.0 1.0 1.0 1.0 1.0 1.0])
+               buffer-object/dynamic-draw)
+  
+  (buffers/draw!
+   context
+   :count 1
+   :shader shader
+            :draw-mode draw-mode/points               
+            :attributes [{:buffer actor_buffer
+                          :location location_pos
+                          :components-per-vertex 4
+                          :type data-type/float
+                          :offset 0
+                          :stride 32}
+                         {:buffer actor_buffer
+                          :location location_col
+                          :components-per-vertex 4
+                          :type data-type/float
+                          :offset 16
+                          :stride 32}]
+            :uniforms [{:name "projection"
+                        :type :mat4
+                        :values projection}]
+            )
+  ;; return state
+  state
+  )
